0596ede912c3178ff9ad23eac8b13daa6a193724
Yepei Zhang <ypzhang@ucdavis.edu>
2016-09-11 12:23:56 -0700

    insert

--- Node.js driver query/insert.js
+++ Node.js driver query/insert.js
@@ -1,4 +1,4 @@
-/*
+// single
 var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/course', function(err, db){
     if(err)  throw err;
@@ -13,7 +13,9 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db){
 
     });
 });
-   */
+
+
+// duplicate
 
 var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/course', function(err, db){
@@ -32,5 +34,23 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db){
     });
 
 });
+
+// multiple
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost:27017/course', function(err, db){
+    if(err)  throw err;
+
+    var docs = [{'student': 'Calvin', 'age': 6},
+                        {'student': 'Susie', 'age': 7} ];
+
+    db.collection('students').insert(docs,function(err, inserted){
+        if(err)  throw err;
+
+        console.dir('Successfully inserted: '+ JSON.stringify(inserted));
+        return db.close();
+
+    });
+});
+
 
 
